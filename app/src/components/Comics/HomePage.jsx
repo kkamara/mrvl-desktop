8574b822272fbbf4580e244ff69c3b27d48287b1
kkamara <kamaracomputers@gmail.com>
2023-06-15 12:13:13 +0100

    App needs work

--- app/src/components/Comics/HomePage.jsx
+++ app/src/components/Comics/HomePage.jsx
@@ -82,6 +82,10 @@ const HomePage = ({
 		!data.results.length)
 
 	const __renderComics = () => {
+		if (true) {
+			return <p>No results to display your query.</p>
+		}
+
 		if (!dataHasLoaded) {
 			return <p>No results to display your query.</p>
 		}
