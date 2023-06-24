e6f41f8131a0d690395334207286db2975e3017d
kkamara <kamaracomputers@gmail.com>
2023-06-24 18:18:27 +0100

    Update footer

--- app/src/redux/actions/pagination.jsx
+++ app/src/redux/actions/pagination.jsx
@@ -1,69 +1,68 @@
 import { paginationActions, } from '../reducers/types'
 
 export function getPagination() {
-    return async (dispatch, getState) => {
-      dispatch(request(paginationActions.GET_PAGINATION_PENDING))
-			const { 
-				data: existingPaginationData,
-			} = getState().pagination 
-			const pagination = localStorage.getItem('pagination')
-			console.log('getting pagination from storage', JSON.parse(pagination))
-	    if (!pagination) {
-				return dispatch(success(
-					paginationActions.GET_PAGINATION_SUCCESS,
-					existingPaginationData,
-	    	))
-			}
-	
+	return async (dispatch, getState) => {
+		dispatch(request(paginationActions.GET_PAGINATION_PENDING))
+		const { 
+			data: existingPaginationData,
+		} = getState().pagination 
+		const pagination = localStorage.getItem('pagination')
+		if (!pagination) {
+			return dispatch(success(
+				paginationActions.GET_PAGINATION_SUCCESS,
+				existingPaginationData,
+			))
+		}
+
+		try {
+			dispatch(success(
+				paginationActions.GET_PAGINATION_SUCCESS,
+				JSON.parse(pagination),	
+			))
+		} catch (err) {
+			console.log(err.message)
+			dispatch(error(
+				paginationActions.GET_PAGINATION_ERROR,
+				err.message,
+			))
+			dispatch(success(
+				paginationActions.GET_PAGINATION_SUCCESS,
+				existingPaginationData,	
+			))
 			try {
+				localStorage.setItem('pagination', JSON.stringify({ offset: 0, }))
 				dispatch(success(
 					paginationActions.GET_PAGINATION_SUCCESS,
-					JSON.parse(pagination),	
-        ))
-	   	} catch (err) {
+					existingPaginationData,
+				))
+			} catch (err) {
 				console.log(err.message)
 				dispatch(error(
 					paginationActions.GET_PAGINATION_ERROR,
 					err.message,
 				))
-				dispatch(success(
-					paginationActions.GET_PAGINATION_SUCCESS,
-					existingPaginationData,	
-      	))
-      	try {
-		        localStorage.setItem('pagination', JSON.stringify({ offset: 0, }))
-		        dispatch(success(
-							paginationActions.GET_PAGINATION_SUCCESS,
-	            existingPaginationData,
-		        ))
-		   	} catch (err) {
-					console.log(err.message)
-					dispatch(error(
-						paginationActions.GET_PAGINATION_ERROR,
-						err.message,
-					))
-		    }
-	    }
-      
-      function request(type) {
-          return {
-              type
-          }
-      }
+			}
+		}
+		
+		function request(type) {
+			return {
+				type,
+			}
+		}
 
-      function error(type, payload) {
-        return {
-            type,
-            payload
-        }
-      }
+		function error(type, payload) {
+			return {
+				type,
+				payload,
+			}
+		}
 
-      function success(type, payload) {
-        return {
-            type,
-            payload
-        }
-      }
+		function success(type, payload) {
+			return {
+				type,
+				payload,
+			}
+		}
   }
 }
 
@@ -76,11 +75,11 @@ export function setPagination(pagination) {
 			...pagination,
 		}
 		try {
-        localStorage.setItem('pagination', JSON.stringify(newPagination))
-        dispatch(success(
-					paginationActions.SET_PAGINATION_SUCCESS,
-          newPagination,
-        ))
+			localStorage.setItem('pagination', JSON.stringify(newPagination))
+			dispatch(success(
+				paginationActions.SET_PAGINATION_SUCCESS,
+				newPagination,
+			))
    	} catch (err) {
 			console.log(err.message)
 			dispatch(error(
@@ -88,25 +87,25 @@ export function setPagination(pagination) {
 				err.message,
 			))
     }
-      
+    
     function request(type) {
-        return {
-            type
-        }
+			return {
+				type,
+			}
     }
     
     function error(type, payload) {
-        return {
-            type,
-            payload
-        }
+			return {
+				type,
+				payload,
+			}
     }
 
     function success(type, payload) {
-        return {
-            type,
-            payload
-        }
+			return {
+				type,
+				payload,
+			}
     }
   }
 }
--- app/src/redux/reducers/pagination.jsx
+++ app/src/redux/reducers/pagination.jsx
@@ -1,60 +1,60 @@
 import { paginationActions, } from "./types"
 
 const initialState = {
-    data: { 
-		  hideFields: false,
-		  format: '',
-		  formatType: '',
-		  noVariants: false,
-		  dateDescriptor: '',
-		  dateRange: '',
-		  title: '',
-		  titleStartsWith: '',
-		  startYear: '',
-		  issueNumber: '',
-		  diamondCode: '',
-		  digitalID: '',
-		  upc: '',
-		  isbn: '',
-		  ean: '',
-		  issn: '',
-		  hasDigitalIssue: '',
-		  modifiedSince: '',
-		  creators: '',
-		  characters: '',
-		  series: '',
-		  events: '',
-		  stories: '',
-		  sharedAppearances: '',
-		  collaborators: '',
-		  orderBy: '',
-		  limit: '',
-			offset: 0,
-		},
-    fetched: false,
-    loading: false,
+	data: { 
+		hideFields: false,
+		format: '',
+		formatType: '',
+		noVariants: false,
+		dateDescriptor: '',
+		dateRange: '',
+		title: '',
+		titleStartsWith: '',
+		startYear: '',
+		issueNumber: '',
+		diamondCode: '',
+		digitalID: '',
+		upc: '',
+		isbn: '',
+		ean: '',
+		issn: '',
+		hasDigitalIssue: '',
+		modifiedSince: '',
+		creators: '',
+		characters: '',
+		series: '',
+		events: '',
+		stories: '',
+		sharedAppearances: '',
+		collaborators: '',
+		orderBy: '',
+		limit: '',
+		offset: 0,
+	},
+	fetched: false,
+	loading: false,
 }
 const paginationReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case paginationActions.GET_PAGINATION_PENDING:
+	switch (action.type) {
+		case paginationActions.GET_PAGINATION_PENDING:
 		case paginationActions.SET_PAGINATION_PENDING:
-            return { ...state, fetched: false, loading: true, }
-        case paginationActions.GET_PAGINATION_ERROR:
+			return { ...state, fetched: false, loading: true, }
+		case paginationActions.GET_PAGINATION_ERROR:
 		case paginationActions.SET_PAGINATION_ERROR:
-            return {
-                ...state,
-                fetched: false,
-                loading: true,
-                error: action.payload
-            }
-        case paginationActions.GET_PAGINATION_SUCCESS:
+			return {
+				...state,
+				fetched: false,
+				loading: true,
+				error: action.payload
+			}
+		case paginationActions.GET_PAGINATION_SUCCESS:
 		case paginationActions.SET_PAGINATION_SUCCESS:
-            return {
-                ...state,
-                fetched: true,
-                loading: false,
-                data: { ...action.payload },
-            }
+			return {
+				...state,
+				fetched: true,
+				loading: false,
+				data: { ...action.payload },
+			}
     }
 
     return state
