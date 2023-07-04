fecd96e8038e435c1e86bd4c27677bde3eb36c2d
Kelvin Kamara <31970496+kkamara@users.noreply.github.com>
2023-07-04 13:35:27 +0100

    Update electron.js

--- app/public/electron.js
+++ app/public/electron.js
@@ -1,11 +1,11 @@
-const electron = require('electron')
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
-const path = require('node:path')
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
 
-const inProduction = app.isPackaged
+const inProduction = app.isPackaged;
 
-let mainWindow
+let mainWindow;
 
 function createWindow() {
     mainWindow = new BrowserWindow({
@@ -17,24 +17,24 @@ function createWindow() {
     })
 
     if (inProduction) {
-        mainWindow.loadURL(`file://${path.join(__dirname, '../build/index.html')}`)
+        mainWindow.loadURL(`file://${path.join(__dirname, '../build/index.html')}`);
     } else {
-        mainWindow.loadURL('http://localhost:3000')
+        mainWindow.loadURL('http://localhost:3000');
     }
 
-    mainWindow.on('closed', () => (mainWindow = null))
+    mainWindow.on('closed', () => (mainWindow = null));
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
-        app.quit()
+        app.quit();
     }
-})
+});
 
 app.on('activate', () => {
     if (mainWindow === null) {
-        createWindow()
+        createWindow();
     }
-})
+});
